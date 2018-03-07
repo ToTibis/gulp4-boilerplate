35461c6104b1eb1d12a56e96469c40acc29f29ba
ToTibis <dumka.tot@gmail.com>
2018-03-07 13:03:52 +0400

    'change-html-task'

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,6 +1,7 @@
 module.exports = function() {
 	$.gulp.task('html', function() {
 		return $.gulp.src($.path.src.html)
+		.pipe($.loadPlugin.changed($.path.build.html))
 		.pipe($.loadPlugin.rigger())
 		.pipe($.gulp.dest($.path.build.html))
 		.on('end', $.browserSync.reload)
