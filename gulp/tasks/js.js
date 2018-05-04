47f16f895b5e0e59ca25077b083e4b54a7e0e243
ToTibis <dumka.tot@gmail.com>
2018-05-04 11:40:24 +0500

    babel-added

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -2,6 +2,9 @@ module.exports = function() {
 	$.gulp.task('js', function() {
 		return $.gulp.src($.path.src.js, {since: $.gulp.lastRun('js')})
 		.pipe($.loadPlugin.rigger())
+		.pipe($.loadPlugin.babel({
+			presets: ['es2015']
+		}))
 		.pipe($.loadPlugin.uglify())
 		.pipe($.gulp.dest($.path.build.js))
 		.pipe($.browserSync.reload({
