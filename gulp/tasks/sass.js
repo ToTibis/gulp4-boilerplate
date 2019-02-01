64209af9e86fbe8d4c7b235ccf9bb60bdfdf5e7d
ToTibis <dumka.tot@gmail.com>
2019-02-01 14:03:58 +0400

    module-resolver added

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -3,11 +3,11 @@ module.exports = function() {
 	$.gulp.task('sass:development', function () {
 		return $.gulp.src($.path.src.sass, {since: $.gulp.lastRun('sass:development')})
 		.pipe($.sourcemaps.init())
-		.pipe($.autoprefixer())
 		.pipe($.aliases({
 			'~modules': 'node_modules'
 		}))
 		.pipe($.sass({outputStyle: 'compact'}).on('error', $.sass.logError))
+		.pipe($.autoprefixer())
 		.pipe($.sourcemaps.write('./'))
 		.pipe($.gulp.dest($.path.build.css))
 		.pipe($.browserSync.stream())
