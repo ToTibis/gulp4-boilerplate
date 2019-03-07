6cd00b57ac745dc24914512b6fdaae0e75e3e113
ToTibis <dumka.tot@gmail.com>
2019-03-07 15:02:31 +0400

    change in sass task

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -15,11 +15,11 @@ module.exports = function() {
 
 	$.gulp.task('sass:production', function () {
 		return $.gulp.src($.path.src.sass)
-		.pipe($.autoprefixer())
 		.pipe($.aliases({
 			'~modules': 'node_modules'
 		}))
 		.pipe($.sass({outputStyle: 'compressed'}).on('error', $.sass.logError))
+		.pipe($.autoprefixer())
 		.pipe($.stripCss())
 		.pipe($.gulp.dest($.path.build.css))
 	});
