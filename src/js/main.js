b04f5b0670f2b4cf8e6c5561d4de69f620f29d33
ToTibis <dumka.tot@gmail.com>
2019-09-12 15:13:22 +0400

    structure refactoring

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,10 @@ import 'svgxuse';
 import 'jquery/src/core/init';
 import 'jquery/src/core/parseHTML';
 import 'jquery/src/manipulation';
-import 'jquery/src/css';
+import 'jquery/src/deferred';
+import 'jquery/src/event';
+import 'jquery/src/selector';
+import 'jquery/src/wrap';
+
 import './modules/common.js';
 
